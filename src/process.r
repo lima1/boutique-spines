1511a813c4d383a31c397d662d43a49c47fbd50f
Markus Riester <markus.riester@novartis.com>
2025-06-27 15:50:07 -0400

    Added Own columns to internal data

--- src/process.r
+++ src/process.r
@@ -2,8 +2,11 @@ library(openxlsx)
 library(data.table)
 
 x <- read.xlsx("../data/radiance.xlsx", startRow=2)
+x$Own <- NULL
 fwrite(x, "../complete/radiance.csv")
 x <- read.xlsx("../data/ignite-films.xlsx", startRow=2)
+x$Own <- NULL
 fwrite(x, "../complete/ignite-films.csv")
 x <- read.xlsx("../data/subkultur-usa.xlsx", startRow=2)
+x$Own <- NULL
 fwrite(x, "../complete/subkultur-usa.csv")
